3705a9038478c0dce5674dbb70c19fd226dffe15
Frederick Ofori <FrederickCodes@gmail.com>
2017-11-18 19:10:51 +0000

    setup todo controller with routes, exported it and required it in app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var todoController = require('./controllers/todoController');
 
 var app = express();
 
@@ -8,6 +9,10 @@ app.set('view engine', 'ejs');
 
 // static files
 app.use(express.static('./public'));
+
+// fire controllers
+todoController(app);
+
 
 // listen to port
 app.listen(3000);
---
+++ controllers/todoController.js
@@ -0,0 +1,18 @@
+// to handle routes, and rendering and passing of data to views 
+module.exports = function(app){
+
+// routes
+
+app.get('/todo', function(req,res){
+
+});
+
+app.post('/todo', function(req,res){
+    
+});
+
+app.delete('/todo', function(req,res){
+    
+});
+
+}
