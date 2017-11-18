c03d6c66c1fb458d94a6ff55a0362a1a6c264f23
Frederick Ofori <FrederickCodes@gmail.com>
2017-11-18 22:50:32 +0000

    POST and DELETE routes added for todo list, jquery added for both

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,17 +1,29 @@
+var bodyParser = require('body-parser');
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}]
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 // to handle routes, and rendering and passing of data to views 
 module.exports = function(app){
 
 // routes
 
 app.get('/todo', function(req,res){
-res.render('todo')
+res.render('todo', {todos: data});
 });
 
-app.post('/todo', function(req,res){
-    
+app.post('/todo', urlencodedParser, function(req,res){
+    data.push(req.body)
+    console.log(data)
+    res.json(data)
 });
 
-app.delete('/todo', function(req,res){
+app.delete('/todo/:item', function(req,res){
+    data = data.filter(function(todo){
+        return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    console.log(data)
+    res.json(data);
     
 });
 
