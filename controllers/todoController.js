41803916bee0d75cb4f2d3792a0c1907d989f24f
Frederick Ofori <FrederickCodes@gmail.com>
2017-11-19 12:44:26 +0000

    database added and connected, todoSchema

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,22 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+/////////
+// Connect to the database
+mongoose.connect('mongodb://test:test@ds161793.mlab.com:61793/freddiecodestodo');
 
+// Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+//
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+var itemOne = Todo({item: "get flowers"}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
+////////
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}]
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
